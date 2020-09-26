61c88d8d24dfe5451cf6ad2ef09bf01f11bd4a21
Ankesh Kedarnath <ankesh.kedarnath@hach.com>
2020-09-27 05:26:34 +0530

    adding some more outputs

--- output.tf
+++ output.tf
@@ -2,3 +2,13 @@ output "az_public_ip_address" {
   description = "The address of the newly created Public IP"
   value       = azurerm_public_ip.az_public_ip.ip_address
 }
+
+output "az_public_ip_id" {
+  description = "The id of the newly created Public IP"
+  value       = azurerm_public_ip.az_public_ip.id
+}
+
+output "az_public_ip_name" {
+  description = "The name of the newly created Public IP"
+  value       = var.name
+}
